9fcf06d874fc858d02eef06dfef4e83c079905b4
joa0neves <unfit4winter@gmail.com>
2021-11-23 20:44:05 +0000

    changes for testing

--- rpc_server_testing.py
+++ rpc_server_testing.py
@@ -1,26 +1,20 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-import csv
 import xml.etree.ElementTree as ET
+import xml.etree
 import convert_to_xml as converter
-import psycopg2
-from lxml import etree
+#from lxml import etree
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 with SimpleXMLRPCServer(('localhost', 9000),requestHandler=RequestHandler) as server:
-    conn=psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="1234")
     server.register_introspection_functions()
     @server.register_function(name='validate')
     def remainder_function(xml):
-        xml_file = lxml.etree.parse(xml)
+        xml_file = xml.etree.parse(xml)
 
-        xml_validator = lxml.etree.XMLSchema(file="schema.xsd")
+        xml_validator = xml.etree.XMLSchema(file="schema.xsd")
 
         is_valid = xml_validator.validate(xml_file)
 
